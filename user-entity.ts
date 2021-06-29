0ab258ffaf1a4b77d4c33db4f972cb951af02fe9
Pedro Teixeira <otmpedrofaria@gmail.com>
2021-06-29 11:40:57 -0300

    Create user-entity.ts

---
+++ user-entity.ts
@@ -0,0 +1,23 @@
+import { PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+abstract class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default User;
