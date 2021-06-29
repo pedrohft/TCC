37df8d62499671ac7d6967cca1c58e7e38fd9047
Pedro Teixeira <otmpedrofaria@gmail.com>
2021-06-29 11:45:58 -0300

    Create migration.ts

---
+++ migration.ts
@@ -0,0 +1,13 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class NomeMigrationTIMESTAMP implements MigrationInterface {
+
+    async up(queryRunner: QueryRunner): Promise<void> {
+
+    }
+
+    async down(queryRunner: QueryRunner): Promise<void> {
+
+    }
+
+}
